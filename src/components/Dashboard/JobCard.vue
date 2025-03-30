f97d8a3eb508808518244dfabda5c6c2d9fba889
celina-ngth <ngeth.celina@gmail.com>
2025-03-30 11:36:32 +0200

    feat: init dashboard with jobs

---
+++ src/components/Dashboard/JobCard.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { Job } from '@/pages/Dashboard.vue'
+
+defineProps<{ job: Job }>()
+</script>
+
+<template>
+	<div
+		class="flex w-full gap-2 rounded-md border border-gray-200 p-3 hover:cursor-pointer hover:bg-gray-100"
+	>
+		{{ job.title }}
+	</div>
+</template>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,47 +3,81 @@ import { useI18n } from 'vue-i18n'
 import Text from '@/components/ui/Text.vue'
 import Button from '@/components/ui/Button.vue'
 import Icon from '@/components/ui/Icon.vue'
+import JobCard from '@/components/Dashboard/JobCard.vue'
+import Draggable from 'vuedraggable'
+import { ref } from 'vue'
 
 const { t } = useI18n()
 
-const STATUS = [
+export interface Job {
+	id: string
+	title: string
+}
+
+interface Column {
+	id: string
+	title: string
+	icon: string
+	jobs: Job[]
+}
+
+const columns = ref<Column[]>([
 	{
+		id: 'wishlist',
+		title: t('dashboard.wishlist.title'),
 		icon: 'Sparkles',
-		name: t('dashboard.wishlist.title')
+		jobs: [
+			{ id: 'job-1', title: 'Google' },
+			{ id: 'job-2', title: 'Facebook' }
+		]
 	},
 	{
+		id: 'applied',
+		title: t('dashboard.applied.title'),
 		icon: 'FileCheck',
-		name: t('dashboard.applied.title')
+		jobs: []
 	},
 	{
+		id: 'interview',
+		title: t('dashboard.interview.title'),
 		icon: 'BriefcaseBusiness',
-		name: t('dashboard.interview.title')
+		jobs: []
 	},
 	{
+		id: 'rejected',
+		title: t('dashboard.rejected.title'),
 		icon: 'ThumbsDown',
-		name: t('dashboard.rejected.title')
+		jobs: []
 	}
-]
+])
 </script>
 
 <template>
 	<Text tag="h1">{{ $t('dashboard.title') }}</Text>
 
-	<div class="grid grid-cols-4 gap-4">
-		<div v-for="(status, index) in STATUS" :key="`${status.name}-${index}`">
+	<div class="flex items-start gap-4">
+		<div v-for="column in columns" :key="column.id" class="flex w-full gap-4">
 			<div
 				class="flex w-full flex-col content-center items-center justify-center gap-2 rounded border border-gray-200 p-3"
 			>
 				<div
 					class="flex w-full content-center items-center justify-center gap-2"
 				>
-					<Icon :name="status.icon" class="mb-2" />
+					<Icon :name="column.icon" class="mb-2" />
 					<Text tag="h4" class="text-center uppercase">
-						{{ status.name }}
+						{{ column.title }}
 					</Text>
 				</div>
 
 				<Button full-width><Icon name="Plus" /></Button>
+
+				<div
+					v-for="job in column.jobs"
+					:key="job.id"
+					class="flex w-full flex-col gap-2"
+				>
+					<JobCard :job="job" />
+				</div>
 			</div>
 		</div>
 	</div>
